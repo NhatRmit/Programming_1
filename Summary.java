02a39b7d429eb63482fc13857365d7c684a18abc
NhatRmit <62049702+NhatRmit@users.noreply.github.com>
2021-08-23 00:49:24 +0700

    Updated

--- Summary.java
+++ Summary.java
@@ -6,15 +6,16 @@ public class Summary {
     int num_of_group, daysInput;
     String path = "java/src/covid-data.csv";
     String line = "";
+    String userMetric = "";
     Scanner sc = new Scanner(System.in);
     ArrayList<String> list = new ArrayList<>();
     ArrayList<String> userList = new ArrayList<>();
-    ArrayList<String> timerange = new ArrayList<>();
-    ArrayList<String> cases = new ArrayList<>();
-    ArrayList<String> deaths = new ArrayList<>();
-    ArrayList<String> vaccinations = new ArrayList<>();
+    ArrayList<String> subList = new ArrayList<>();
+    ArrayList<String> metric = new ArrayList<>();
+    ArrayList<Integer> newtotal = new ArrayList<>();
+    ArrayList<Integer> upto = new ArrayList<>();
     List<List<String>> dataList = new ArrayList<>();
-    
+
     String[] userData;
     Data user = new Data();
 
@@ -31,29 +32,45 @@ public class Summary {
             list.add(line);
         }
 
-        System.out.println(list.get(0));
-
         for (String e : list) {
+            if (isContain(e, user.area) == true) {
+                subList.add(e);
+            }
+        }
+
+        for (String e : subList) {
             for (int i = 0; i < user.range.length; i++) {
-                if (isContain(e, user.range[i]) == true && isContain(e, user.area) == true) {
-                    timerange.add(user.range[i]);
+                if (isContain(e, user.range[i]) == true) {
                     userList.add(e);
                 }
             }
         }
+    }
 
+    public void convertToArray(ArrayList<String> userList2) {
         // convert list to array
-        String[] userListConvert = new String[userList.size()];
-        userListConvert = userList.toArray(userListConvert);
+        String[] userListConvert = new String[userList2.size()];
+        userListConvert = userList2.toArray(userListConvert);
 
         String[] value;
 
         // get each index
         for (String s : userListConvert) {
             value = s.split(",");
-            cases.add(value[4]);
-            deaths.add(value[5]);
-            vaccinations.add(value[6]);
+            switch (this.userMetric) {
+                case "cases":
+                    this.metric.add(value[4]);
+                    break;
+                case "deaths":
+                    this.metric.add(value[5]);
+                    break;
+                case "vaccinations":
+                    this.metric.add(value[6]);
+                    break;
+                default:
+                    annInvalid();
+                    break;
+            }
         }
     }
 
@@ -64,7 +81,7 @@ public class Summary {
                 1. No Grouping
                 2. Number of Groups
                 3. Number of Days in a group""");
-                
+
         opt = sc.nextInt();
         switch (opt) {
             case 1:
@@ -81,28 +98,22 @@ public class Summary {
                 inputMetric(opt);
                 break;
             default:
+                annInvalid();
                 groupData();
         }
     }
 
-    public void noGrouping(ArrayList<String> metric) {
+    public void noGrouping() {
         this.dataList = createDataList(userList.size());
-            
-                for (int i = 0; i < userList.size(); i++) {
-                    for (int j = 1; j <= 1; j++) {
-                        (this.dataList.get(i)).add(metric.get((j - 1) + i));
-                    }
-                }
-                
-        
+        for (int i = 0; i < userList.size(); i++) {
+            for (int j = 1; j <= 1; j++) {
+                (this.dataList.get(i)).add(this.metric.get((j - 1) + i));
+            }
+        }
         displayGrouping(this.dataList);
     }
 
-    public void calnumofgroup(ArrayList<String> name, int num_of_group, int qty_days, int checkdays) {
-        
-    }
-
-    public void numOfGroups(ArrayList<String> metric) {
+    public void numOfGroups() {
 
         int qty_days = userList.size() / this.num_of_group;
         int checkdays = userList.size() % this.num_of_group;
@@ -111,53 +122,71 @@ public class Summary {
             if (checkdays <= userList.size()) {
                 this.dataList = createDataList(this.num_of_group);
                 int count = 0;
-        int pos_datalist = checkdays;
-        for (int i = 0; i < this.num_of_group; i++) {
-            int value = i * qty_days;
-            if (i == this.num_of_group - pos_datalist) {
-                for (int j = 1; j <= qty_days + 1; j++) {
-                    (dataList.get(i)).add(metric.get((j - 1) + value + count));
+                int pos_datalist = checkdays;
+                for (int i = 0; i < this.num_of_group; i++) {
+                    int value = i * qty_days;
+                    if (i == this.num_of_group - pos_datalist) {
+                        for (int j = 1; j <= qty_days + 1; j++) {
+                            (dataList.get(i)).add(this.metric.get((j - 1) + value + count));
+                        }
+                        count++;
+                        pos_datalist--;
+                    } else {
+                        for (int j = 1; j <= qty_days; j++) {
+                            (this.dataList.get(i)).add(this.metric.get((j - 1) + value));
+                        }
+                    }
                 }
-                count++;
-                pos_datalist--;
-            } else {
-                for (int j = 1; j <= qty_days; j++) {
-                    (this.dataList.get(i)).add(metric.get((j - 1) + value));
-                }
-            }
-        }
             }
             displayGrouping(this.dataList);
         } else {
-            System.out.println("invalid");
-            numOfGroups(metric);
+            annInvalid();
+            numOfGroups();
         }
     }
 
-    public void numOfDays(ArrayList<String> metric) {
+    public void numOfDays() {
         this.num_of_group = userList.size() / this.daysInput; // N.O groups in case % == 0
         // Check
         if (userList.size() % this.daysInput == 0) {
             this.dataList = createDataList(this.num_of_group);
             int flag = 0;
-            
-                
-                    for (int i = 0; i < this.dataList.size(); i++) {
-                        for (int j = 0; j < this.daysInput; j++) {
-                            (this.dataList.get(i)).add(metric.get(flag));
-                            flag++;
-                        }
-                    }
-            
+
+            for (int i = 0; i < this.dataList.size(); i++) {
+                for (int j = 0; j < this.daysInput; j++) {
+                    (this.dataList.get(i)).add(this.metric.get(flag));
+                    flag++;
+                }
+            }
             displayGrouping(this.dataList);
         } else {
-            System.out.println("INVALID");
-            numOfDays(metric);
+            annInvalid();
+            numOfDays();
         }
     }
 
+    public void runMetric(int num) {
+        convertToArray(this.userList);
+        switch (num) {
+            case 1:
+                noGrouping();
+                chooseResultType();
+                break;
+            case 2:
+                numOfGroups();
+                chooseResultType();
+                break;
+            case 3:
+                numOfDays();
+                chooseResultType();
+                break;
+            default:
+                annInvalid();
+                runMetric(num);
+        }
+    }
 
-    public int inputMetric(int num) {
+    public void inputMetric(int num) {
         System.out.println("""
                 Select the information you want to see:
                 1. New Cases
@@ -166,78 +195,44 @@ public class Summary {
         int opt = sc.nextInt();
         switch (opt) {
             case 1:
-                switch (num) {
-                    case 1:
-                        noGrouping(cases);
-                        chooseResultType(cases);
-                        break;
-                    case 2:
-                        numOfGroups(cases);
-                        chooseResultType(cases);
-                        break;
-                    case 3: 
-                        numOfDays(cases);
-                        chooseResultType(cases);
-                        break;
-                }
+                this.userMetric = "cases";
+                runMetric(num);
                 break;
-            case 2: 
-                switch (num) {
-                    case 1:
-                        noGrouping(deaths);
-                        chooseResultType(deaths);
-                        break;
-                    case 2:
-                        numOfGroups(deaths);
-                        chooseResultType(deaths);
-                        break;
-                    case 3: 
-                        numOfDays(deaths);
-                        chooseResultType(deaths);
-                        break;
-                }       
-            case 3: 
-                switch (num) {
-                    case 1:
-                        noGrouping(vaccinations);
-                        chooseResultType(vaccinations);
-                        break;
-                    case 2:
-                        numOfGroups(vaccinations);
-                        chooseResultType(vaccinations);
-                        break;
-                    case 3: 
-                        numOfDays(vaccinations);
-                        chooseResultType(vaccinations);
-                        break;
-                }       
-                 
+            case 2:
+                this.userMetric = "deaths";
+                runMetric(num);
+                break;
+            case 3:
+                this.userMetric = "vaccinations";
+                runMetric(num);
+                break;
             default:
+                annInvalid();
+                inputMetric(num);
                 break;
         }
-        return 1;
+
     }
 
-    public ArrayList<Integer> newTotal(ArrayList<String> metric) {
-        ArrayList<Integer> newtotal = new ArrayList<>();
+    public ArrayList<Integer> newTotal() {
         // go through each group after grouping
         for (int i = 0; i < this.dataList.size(); i++) {
             int sum = 0;
-            if(metric != vaccinations) {
+            if (this.userMetric != "vaccinations") {
                 for (String str : this.dataList.get(i)) {
                     int num = Integer.parseInt(str);
                     sum += num;
                 }
             } else {
-                for (int j = 0 ; j < this.dataList.get(i).size(); j++) {
+                for (int j = 0; j < this.dataList.get(i).size(); j++) {
                     int first_num = Integer.parseInt((this.dataList.get(i)).get(0));
-                    int last_num = Integer.parseInt((this.dataList.get(i)).get(this.dataList.get(i).size()-1));
+                    int last_num = Integer.parseInt((this.dataList.get(i)).get(this.dataList.get(i).size() - 1));
                     sum = last_num - first_num;
                 }
             }
-            newtotal.add(sum);
+            this.newtotal.add(sum);
         }
-        return newtotal;
+        return this.newtotal;
     }
 
     public int upTo() {
@@ -245,7 +240,7 @@ public class Summary {
         return -1;
     }
 
-    public void chooseResultType(ArrayList<String> metric) {
+    public void chooseResultType() {
         System.out.println("""
                 Select the the result type:
                 1. New Total
@@ -253,14 +248,14 @@ public class Summary {
         int option = sc.nextInt();
         switch (option) {
             case 1:
-                System.out.println(newTotal(metric));
+                System.out.println(newTotal());
                 break;
             case 2:
                 System.out.println(upTo());
                 break;
             default:
-                System.out.println("Invalid!");
-                chooseResultType(metric);
+                annInvalid();
+                chooseResultType();
         }
     }
 
@@ -284,5 +279,9 @@ public class Summary {
         Pattern p = Pattern.compile(pattern);
         Matcher m = p.matcher(source);
         return m.find();
+    }
+
+    public void annInvalid() {
+        System.out.println("Your input is INVALID. Please, input again!");
     }
 }
