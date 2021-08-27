3c1222f7a69337e33ac3b390378fa226e75c7683
NhatRmit <62049702+NhatRmit@users.noreply.github.com>
2021-08-27 14:47:19 +0700

    Update Display

--- java/src/Display.java
+++ java/src/Display.java
@@ -6,84 +6,100 @@ public class Display {
     static Summary s = new Summary();
 
     public void tableWithLinesAndMaxWidth() throws FileNotFoundException, Exception {
-        boolean leftJustifiedRows = true;
-        int maxWidth = 30;
+        boolean rowJustifyLeft = true;
+        int tableMaxWidth = 80;
         s.summary();
 
-        String[][] table = new String[s.uptoString.size()][2];
+        String[][] tabular = new String[(s.resultString.size())+1][2];
+        tabular[0][0] = "Range";
+        tabular[0][1] = "Value";
 
-        for (int i = 0; i < table.length; i++) {
-            table[i][0] = s.dataTimeString.get(i);
+        for (int i = 1; i < tabular.length; i++) {
+            tabular[i][0] = s.dataTimeString.get(i-1);
         }
 
-        for (int i = 0; i < table.length; i++) {
-            table[i][1] = s.uptoString.get(i);
+        for (int i = 1; i < tabular.length; i++) {
+            tabular[i][1] = s.resultString.get(i-1);
         }
 
-        // ArrayList<String[]> listTablePrep = new ArrayList<>(Arrays.asList(table));
-        // ArrayList<String[]> listTablePrint = new ArrayList<>();
-        // for (String[] row : listTablePrep) {
-        //     boolean needExtraRow = false;
-        //     int splitRow = 0;
-        //     do {
-        //         needExtraRow = false;
-        //         String[] newRow = new String[row.length];
-        //         for (int i = 0; i < row.length; i++) {
-        //             if (row[i].length() < maxWidth) {
-        //                 newRow[i] = splitRow == 0 ? row[i] : "";
-        //             } else if ((row[i].length() > (splitRow * maxWidth))) {
-        //                 int end = row[i].length() > ((splitRow * maxWidth) + maxWidth)
-        //                         ? (splitRow * maxWidth) + maxWidth
-        //                         : row[i].length();
-        //                 newRow[i] = row[i].substring((splitRow * maxWidth), end);
-        //                 needExtraRow = true;
-        //             } else {
-        //                 newRow[i] = "";
-        //             }
-        //         }
-        //         listTablePrint.add(newRow);
-        //         if (needExtraRow) {
-        //             splitRow++;
-        //         }
-        //     } while (needExtraRow);
-        // }
+        ArrayList<String[]> table = new ArrayList<>(Arrays.asList(tabular));
+        ArrayList<String[]> tableFinale = new ArrayList<>();
+        for (String[] row : table) {
+            boolean extraRow = false;
+            int split = 0;
+            do {
+                extraRow = false;
+                String[] addRow = new String[row.length];
+                for (int i = 0; i < row.length; i++) {
+                    if (row[i].length() < tableMaxWidth) {
+                        addRow[i] = split == 0 ? row[i] : "";
+                    } else if ((row[i].length() > (split * tableMaxWidth))) {
+                        int end = row[i].length() > ((split * tableMaxWidth) + tableMaxWidth)
+                                ? (split * tableMaxWidth) + tableMaxWidth
+                                : row[i].length();
+                                addRow[i] = row[i].substring((split * tableMaxWidth), end);
+                        extraRow = true;
+                    } else {
+                        addRow[i] = "";
+                    }
+                }
+                tableFinale.add(addRow);
+                if (extraRow) {
+                    split++;
+                }
+            } while (extraRow);
+        }
 
-        // String[][] display = new String[listTablePrint.size()][listTablePrint.get(0).length];
-        // for (int i = 0; i < display.length; i++) {
-        //     display[i] = listTablePrint.get(i);
-        // }
+        String[][] getTable = new String[tableFinale.size()][tableFinale.get(0).length];
+        for (int i = 0; i < getTable.length; i++) {
+            getTable[i] = tableFinale.get(i);
+        }
 
-        // HashMap<Integer, Integer> columnLengths = new HashMap<>();
-        // Arrays.stream(display).forEach(a -> Stream.iterate(0, (i -> i < a.length), (i -> ++i)).forEach(i -> {
-        //     if (columnLengths.get(i) == null) {
-        //         columnLengths.put(i, 0);
-        //     }
-        //     if (columnLengths.get(i) < a[i].length()) {
-        //         columnLengths.put(i, a[i].length());
-        //     }
-        // }));
+        HashMap<Integer, Integer> colLength = new HashMap<>();
+        Arrays.stream(getTable)
+            .forEach(thisTable -> Stream.iterate(0, (i -> i < thisTable.length), (i -> ++i))
+            .forEach(i -> {
+                    if (colLength.get(i) == null) {
+                        colLength.put(i, 0);
+                    }
+                    if (colLength.get(i) < thisTable[i].length()) {
+                        colLength.put(i, thisTable[i].length());
+                    }}
+            ));
 
-        // final StringBuilder formatString = new StringBuilder("");
-        // String flag = leftJustifiedRows ? "-" : "";
-        // columnLengths.entrySet().stream().forEach(e -> formatString.append("| %" + flag + e.getValue() + "s "));
-        // formatString.append("|\n");
+        final StringBuilder getStringFormatter = new StringBuilder("");
 
-        // String line = columnLengths.entrySet().stream().reduce("", (ln, b) -> {
-        //     String templn = "+-";
-        //     templn = templn + Stream.iterate(0, (i -> i < b.getValue()), (i -> ++i)).reduce("", (ln1, b1) -> ln1 + "-",
-        //             (a1, b1) -> a1 + b1);
-        //     templn = templn + "-";
-        //     return ln + templn;
-        // }, (a, b) -> a + b);
-        // line = line + "+\n";
+        String flag = rowJustifyLeft ? "-" : "";
 
-        // System.out.print(line);
-        // Arrays.stream(display).limit(1).forEach(a -> System.out.printf(formatString.toString(), a));
+        colLength.entrySet().stream().forEach(e -> getStringFormatter.append("| %" + flag + e.getValue() + "s "));
+        
+        getStringFormatter.append("|\n");
 
-        // Stream.iterate(1, (i -> i < display.length), (i -> ++i))
-        //         .forEach(a -> System.out.printf(formatString.toString(), display[a]));
-        // System.out.print(line);
+        String getHoriz = colLength.entrySet().stream()
+        .reduce("", (line, b) -> {
+            String lineMaterial = "--";
+            lineMaterial = lineMaterial + Stream.iterate(0, (i -> i < b.getValue()), (i -> ++i)).reduce("", (line1, b1) -> line1 + "-", (a1, b1) -> a1 + b1);
+            lineMaterial = lineMaterial + "-";
+            return line + lineMaterial;
+        }, (a, b) -> a + b);
+
+        getHoriz = getHoriz + "\n";
+        
+        String horizLine = getHoriz;
+
+        System.out.print(horizLine);
+        
+        Arrays.stream(getTable).limit(1).forEach(temp -> System.out.printf(getStringFormatter.toString(), temp));
+
+        System.out.print(horizLine);
+
+        Stream.iterate(1, (i -> i < getTable.length), (i -> ++i))
+                .forEach(temp -> {
+                    System.out.printf(getStringFormatter.toString(), getTable[temp]);
+                    System.out.print(horizLine);
+                });
         
     }
 }
+
 
