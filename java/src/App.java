6a16fbcb39e97024c8419d49347e111b37dafd37
NhatRmit <62049702+NhatRmit@users.noreply.github.com>
2021-08-30 22:20:14 +0700

    Delete java directory

--- java/src/App.java
+++
@@ -1,9 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception  {
-        //Create Instances
-        Interface i = new Interface();
-        i.userInterface();
-    }
-
-}
-
